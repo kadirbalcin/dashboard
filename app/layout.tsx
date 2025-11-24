e66dbe27e5b02897cdddcab29c67d5776c9b3c9f
Kadir Balçın <kadirbalcin145@gmail.com>
2025-11-24 06:52:50 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata = {
   description: "Next.js Process Monitoring Panel"
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
